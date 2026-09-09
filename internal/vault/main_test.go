57f9865069b0d2ba3b30831bf09c43feb7ede896
Daniel Justus <88160656+danieljustus@users.noreply.github.com>
2026-09-09 15:02:30 +0200

    ci: repair native storage and security gates

--- internal/vault/main_test.go
+++ internal/vault/main_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	if runtime.GOOS == "windows" {
+	if runtime.GOOS == "windows" && os.Getenv("SYMVAULT_RUN_WINDOWS_CROSSLANG") != "1" {
 		return // skip vault tests on Windows: LockFileEx access violation in AcquireWriteLock
 	}
 	os.Exit(m.Run())
